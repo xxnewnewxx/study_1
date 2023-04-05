32c102ae014b5718836ddda1ab6dc12a4f586358
xxnewnewxx <xxnewnewxx0127@gmail.com>
2023-04-05 22:09:37 +0900

    first commit

---
+++ object_test01.js
@@ -0,0 +1,27 @@
+// {
+// 객체 선언
+// 나머지 문제들 전부 해당 객체를 복사 붙여넣기해서 사용합니다
+// 각각의 id와 name의 값으로 '웹페', '이지' ,'단순' 을 가지고 있는 객체를 만드시오
+// }
+
+// {
+//   객체 접근
+//   console.log(output_data); // 이지
+// }
+
+let site_1 = {
+    id : '1'
+    name : '웹페'
+};
+
+let site_2 = {
+    id : '1'
+    name : '이지'
+};
+
+let site_3 = {
+    id : '1'
+    name : '단순'
+};
+
+console.log(site_1.name);
