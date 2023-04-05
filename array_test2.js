f69bda126dff1ab8a0576805e465d99b8bf0de11
xxnewnewxx <xxnewnewxx0127@gmail.com>
2023-04-05 21:54:57 +0900

    first commit

--- array_test2.js
+++ array_test2.js
@@ -1,6 +1,10 @@
 // 문제  -- 인덱스 번호 3이 출력되도록 하세요
 
 let input_Data = [10, 20, 30, 40, 50];
+
+let newinput_Data = input_Data.forEach((newarr, index, array) => {
+  console.log(`index: ${index[3]}`);
+});
 console.log;
 
 // 값이 아니라, 인덱스 번호 = 주소값만? 출력하게를 잘모르겠네요 ㅠㅠ
