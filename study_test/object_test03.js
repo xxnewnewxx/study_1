3ad906f36ff15652009ae63abc350b25dbcdc0c3
xxnewnewxx <xxnewnewxx0127@gmail.com>
2023-04-06 22:42:46 +0900

    first commit

---
+++ study_test/object_test03.js
@@ -0,0 +1,35 @@
+// 1번 문제
+// {id: 5, name: "이지형", age: 19, height: 200} 데이터를 추가하기
+
+const users = [
+  {
+    id: 1,
+    name: "문다은",
+    age: 20,
+    height: 165,
+  },
+  {
+    id: 2,
+    name: "윤진섭",
+    age: 24,
+    height: 177,
+  },
+  {
+    id: 3,
+    name: "윤국현",
+    age: 22,
+    height: 175,
+  },
+  {
+    id: 4,
+    name: "배상아",
+    age: 23,
+    height: 168,
+  },
+];
+
+(users.id = "5"),
+  (users.name = "이지형"),
+  (users.age = "19"),
+  (user.height = "200"),
+  console.log(users);
