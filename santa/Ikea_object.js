4bc762e080f4765b718c9df88e95dd9039854858
xxnewnewxx <xxnewnewxx0127@gmail.com>
2023-04-07 22:47:05 +0900

    first commit

---
+++ santa/Ikea_object.js
@@ -0,0 +1,46 @@
+// 이케아 어린이침대 객체정보 정리
+
+let bed_1 = {
+  product: "슬렉트",
+  price: "349000",
+  size_height: "200",
+  ize_width: "90",
+  like_num: "4",
+  color: "white",
+  max_age: "10",
+  delivery: "true",
+  assembly: "true",
+  drawer: "4",
+  Material: "wood",
+  nation: "china",
+};
+
+let bed_2 = {
+  product: "쿠라",
+  price: "225000",
+  size_height: "180",
+  ize_width: "100",
+  like_num: "6",
+  color: "pink",
+  max_age: "10",
+  delivery: "true",
+  assembly: "true",
+  drawer: "4",
+  Material: "wood",
+  nation: "japan",
+};
+
+let bed_3 = {
+  product: "MINNEN",
+  price: "20000",
+  size_height: "200",
+  ize_width: "80",
+  like_num: "3",
+  color: "black",
+  max_age: "10",
+  delivery: "true",
+  assembly: "true",
+  drawer: "2",
+  Material: "wood",
+  nation: "taiwan",
+};
--- study_test/object_test03.js
+++ study_test/object_test03.js
@@ -14,12 +14,14 @@ const users = [
     age: 24,
     height: 177,
   },
+
   {
     id: 3,
     name: "윤국현",
     age: 22,
     height: 175,
   },
+
   {
     id: 4,
     name: "배상아",
@@ -28,8 +30,57 @@ const users = [
   },
 ];
 
-(users.id = "5"),
-  (users.name = "이지형"),
-  (users.age = "19"),
-  (user.height = "200"),
-  console.log(users);
+const newUser = {
+  ...users,
+  4: { id: 5, name: "이지형", age: 19, height: 200 },
+};
+console.log(newUser);
+
+// 문제. 배상아 객체를 출력하세요
+
+console.log(users[3]);
+
+{
+  const result_user = users.filter((item) => item.name == "배상아");
+  console.log(result_user);
+}
+{
+  const result_user_ = users.map((item) => {
+    if (item.name == "배상아") return item;
+    return;
+  });
+  console.log(result_user_);
+}
+
+// 3번 문제
+// 객체의 해당 데이터 값만 수정하기
+// 데이터중 name이 '윤진섭'에 해당하는 height만 190으로 데이터를 변경하고
+// 나머지 데이터들까지 모두 출력한다.
+
+{
+  const result_user = users.map((item) => {
+    if (item.name == "윤진섭") {
+      item.height = 190;
+    }
+
+    return;
+  });
+  return;
+  console.log(result_user);
+}
+
+// 4번 문제
+// height가 170 이하인 user만 출력하시오
+{
+  const result_user = users.filter((item) => item.height >= 170);
+  return;
+}
+console.log(result_user);
+// 5번 문제 (심화)
+// name의 첫 글자가 '윤'으로 시작하는 이름만 출력하시오
+
+/*
+[
+  { id: 2, name: '윤진섭', age: 24, height: 177 },
+  { id: 3, name: '윤국현', age: 22, height: 175 }
+] */
