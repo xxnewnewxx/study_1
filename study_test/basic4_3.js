0e5e2761c0e1a1666827016b8780954e06d1c0d0
xxnewnewxx <xxnewnewxx0127@gmail.com>
2023-04-17 21:47:25 +0900

    first commit

---
+++ study_test/basic4_3.js
@@ -0,0 +1,49 @@
+// // {
+// //   객체 값 변경
+// //   2번째 id의 값을 '이퍼'로 변경하시오
+// // }
+
+// site_2.name = "이퍼";
+
+// console.log(site_2.name);
+
+// site_2.name = "산타";
+
+// console.log(site_2.name);
+
+// // 문제1  -- 웹페이지가 출력되도록하세요
+
+// {
+//   let input_data = ["웹페", "이지", "단순", "한보", "조기"];
+
+//   console.log(output_data); // 웹페이지
+//   //   output = 웹페이지
+// }
+
+// let input_data = ["웹페", "이지", "단순", "한보", "조기"];
+
+// console.log(input_data[0], input_data[1]);
+
+// // 문제 for문을 사용하여 11 12 13 14 15 가 출력되도록하세요
+// {
+//   // for 사용
+//   let input_data = [1, 2, 3, 4, 5];
+//   console.log(input_data); // [11,12,13,14,15]
+// }
+
+let n_num = [1, 2, 3, 4, 5];
+for (let i = 1; i < 6; i++) {
+  console.log(i + 10);
+}
+
+// 문제 출력 30, 40이 나오게하세요
+// {
+//   let input_data = [10, 20, 30, 40, 50];
+//   console.log(output_data); // [30, 40]
+// }
+
+// // 오늘 날씨는 매우 흐림 이라고 나오게 출력되게하세요
+// {
+//   let input_data = "오늘 날씨는 매우 맑음";
+//   console.log(output_data); // ["오늘", "날씨는", "매우", "흐림"]
+// }
